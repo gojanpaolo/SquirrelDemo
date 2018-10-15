17c56043811b7d21d8ec5e4f25f364ebb01a21f9
gojanpaolo <go.janpaolo@gmail.com>
2018-10-14 20:35:08 -0500

    Initial Commit

---
+++ src/MyApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Squirrel;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/gojanpaolo/SquirrelDemo"))
+                    {
+                        await mgr.Result.UpdateApp();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        exception.Text = e.ToString();
+                    });
+                }
+            });
+
+            var assembly = Assembly.GetExecutingAssembly();
+            location.Text = assembly.Location;
+            version.Text = assembly.GetName().Version.ToString(3);
+        }
+    }
+}
