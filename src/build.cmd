5325ef6d790c36b13185ad35ba23761f7f0e2f08
Jan Paolo Go <go.janpaolo@gmail.com>
2019-05-10 05:59:26 -0500

    update script

---
+++ src/build.cmd
@@ -0,0 +1,1 @@
+powershell -ExecutionPolicy Bypass %~dp0\build.ps1
--- src/build.ps1
+++ src/build.ps1
@@ -1,8 +1,7 @@
 function Get-AssemblyVersion($assemblyInfoPath) {
-  #TODO enhance algorithm to ignore comments
-  $pattern = '\[assembly: AssemblyVersion\("(.*)"\)\]'
-  (Get-Content $assemblyInfoPath) | ForEach-Object{
-    if($_ -match $pattern) {
+  $pattern = '^\[assembly: AssemblyVersion\("(.*)"\)\]'
+  (Get-Content $assemblyInfoPath) | ForEach-Object {
+    if ($_ -match $pattern) {
       $assemblyVersionAttribute = [version]$matches[1]
       return "{0}.{1}.{2}" -f $assemblyVersionAttribute.Major, $assemblyVersionAttribute.Minor, $assemblyVersionAttribute.Build
     }
@@ -12,10 +11,7 @@ function Get-AssemblyVersion($assemblyInfoPath) {
 $nuget = ".\nuget.exe"
 
 $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-$msbuild = & $vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
-if ($msbuild) {
-  $msbuild = join-path $msbuild 'MSBuild\15.0\Bin\MSBuild.exe'
-}
+$msbuild = & $vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
 
 $myAppProject = "MyApp\MyApp.csproj"
 
@@ -23,16 +19,11 @@ $myAppProject = "MyApp\MyApp.csproj"
 
 & $msbuild $myAppProject /p:Configuration=Release /p:AllowedReferenceRelatedFileExtensions=.pdb
 
-# TODO change .nuspec path
-# TODO change .nupkg path (or .gitignore)
-# TODO change Releases path
-
 $assemblyVersion = Get-AssemblyVersion "MyApp\Properties\AssemblyInfo.cs"
 
 & $nuget pack MyApp.nuspec -version $assemblyVersion
 
-# TODO if squirrel.windows version is updated
-$squirrel = "packages\squirrel.windows.1.9.0\tools\Squirrel.exe"
+$squirrel = ".\packages\squirrel.windows.*\tools\Squirrel.exe"
 
 & $squirrel --releasify "MyApp.$assemblyVersion.nupkg" --no-msi
 
