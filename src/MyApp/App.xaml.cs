61c7d083e0d3e32ac60d1c7ac4bfcdf54d5e800e
Jan Paolo Go <go.janpaolo@gmail.com>
2019-05-13 20:15:32 -0500

    initial commit

--- src/MyApp/App.xaml.cs
+++ src/MyApp/App.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using Squirrel;
-using System.Threading.Tasks;
+using System;
 using System.Windows;
 
 namespace MyApp
@@ -13,20 +13,45 @@ namespace MyApp
         {
             base.OnStartup(e);
 
-            using (var mgr = new UpdateManager(default))
-            {
-                SquirrelAwareApp.HandleEvents(
-                    onInitialInstall: v =>
-                    {
-                        mgr.CreateShortcutForThisExe();
-                        mgr.CreateRunAtWindowsStartupRegistry();
-                    },
-                    onAppUninstall: v =>
-                    {
-                        mgr.RemoveShortcutForThisExe();
-                        mgr.RemoveRunAtWindowsStartupRegistry();
-                    });
-            }
+            //using (var mgr = new UpdateManager(default))
+            //{
+            //    SquirrelAwareApp.HandleEvents(
+            //        onInitialInstall: v =>
+            //        {
+            //            mgr.CreateShortcutForThisExe();
+            //            mgr.CreateRunAtWindowsStartupRegistry();
+            //        },
+            //        onAppUninstall: v =>
+            //        {
+            //            mgr.RemoveShortcutForThisExe();
+            //            mgr.RemoveRunAtWindowsStartupRegistry();
+            //        });
+            //}
+
+            var rootDirectory =
+                $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\";
+            var updateManager = new UpdateManager(@"C:\git\SquirrelDemo\src\Releases", "MyApp", rootDirectory);
+            SquirrelAwareApp.HandleEvents(
+                onInitialInstall: v =>
+                {
+                    updateManager.CreateShortcutForThisExe();
+                    updateManager.CreateUninstallerRegistryEntry();
+                },
+                onAppUpdate: v =>
+                {
+                    //updateManager.RemoveShortcutForThisExe();
+                    updateManager.CreateShortcutForThisExe();
+                    //updateManager.RemoveUninstallerRegistryEntry();
+                    updateManager.CreateUninstallerRegistryEntry();
+                    //UpdateManager.RestartApp();
+                },
+                onAppUninstall: v =>
+                {
+                    MessageBox.Show("Uninstall started");
+                    updateManager.RemoveShortcutForThisExe();
+                    updateManager.RemoveUninstallerRegistryEntry();
+                });
+
         }
     }
 }
