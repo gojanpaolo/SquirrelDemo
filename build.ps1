2fd3ddebf7a56b30e56d1612a9e6cd052aa48e3d
Jan Paolo Go <go.janpaolo@gmail.com>
2020-08-01 18:23:35 -0500

    backup

--- build.ps1
+++ build.ps1
@@ -17,13 +17,8 @@ $squirrel = ".\packages\squirrel.windows.*\tools\Squirrel.exe"
 $myAppProject = "MyApp\MyApp.csproj"
 $assemblyVersion = Get-AssemblyVersion "MyApp\Properties\AssemblyInfo.cs"
 
-& $nuget restore $myAppProject -SolutionDirectory ".\"
-& $msbuild $myAppProject /p:Configuration=Release /p:AllowedReferenceRelatedFileExtensions=.pdb
 & $nuget pack MyApp.nuspec -version $assemblyVersion
 & $nuget pack MyAppTwo.nuspec -version $assemblyVersion
 & $nuget pack MyDiffApp.nuspec -version $assemblyVersion
-& $squirrel --releasify "MyApp.$assemblyVersion.nupkg"
-& $squirrel --releasify "MyAppTwo.$assemblyVersion.nupkg" -r "ReleasesTwo"
-& $squirrel --releasify "MyDiffApp.$assemblyVersion.nupkg" -r "ReleasesDiff"
 
 pause
